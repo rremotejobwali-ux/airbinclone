2cf1f0444721a190b44e45deaa8a554fea752314
rremotejobwali-ux <rremotejobwali@gmail.com>
2026-03-06 10:38:46 +0500

    Create auth.php

---
+++ auth.php
@@ -0,0 +1,27 @@
+<?php
+// auth.php
+session_start();
+
+function isLoggedIn() {
+    return isset($_SESSION['user_id']);
+}
+
+function requireLogin() {
+    if (!isLoggedIn()) {
+        header("Location: login.php");
+        exit;
+    }
+}
+
+function getCurrentUser() {
+    if (isLoggedIn()) {
+        return [
+            'id' => $_SESSION['user_id'],
+            'name' => $_SESSION['user_name'],
+            'email' => $_SESSION['user_email'],
+            'profile_pic' => $_SESSION['user_pic'] ?? ''
+        ];
+    }
+    return null;
+}
+?>
